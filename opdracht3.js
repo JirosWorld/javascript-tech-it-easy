b97abca4f1161e029bfdd777b9a3fed5db66addb
jolarti <jolarti@gmail.com>
2021-10-30 18:24:01 +0200

    Opdracht 3 - in los JS bestand

--- opdracht3.js
+++ opdracht3.js
@@ -3,11 +3,24 @@
 // Opdracht 3: Gebruik een array-methode om alle tv merken (zoals Philips, NIKKEI, etc.) in een lijst op de pagina weer te geven. Zorg ervoor dat dit ook zou werken als we 200 tv's in onze array zouden hebben staan. Dat er dubbele namen in zitten, is niet erg.
 
 // pseudocode: we willen een nieuwe array maken die álle waarden van 1 attribuut van de oorspronkelijke toont, dus: Map methode.
+// het resultaat van de Map methode stop ik in een variabele 'output_brand'
 const output_brand = inventory.map((brandname) => {
     return brandname.brand;
 });
 console.log('* TV merken (8 resultaten met dubbele namen):',output_brand);
 
-const brand_Dashboard = document.getElementById("brand");
+//niet-nette methode, met alle namen achterelkaar:
+// pseudocode: maak eerst tags in het HTML bestand, waarvan 1 een ID='brandlist' heeft
+const brand_Dashboard = document.getElementById("brandlist");
 console.log(brand_Dashboard);
+// om deze variabele te tonen in de HTML, gebruik ik de .textContent methode
+// resulteert in een niet-nette regel, met alle namen achterelkaar:
 brand_Dashboard.textContent = output_brand;
+
+//kan ook netter door er echt een lijst van te maken:
+//zet elke merknaam in een <p> element. Zorg daarna dat deze in de juiste node komt met appendChild
+output_brand.forEach(brandname => {
+    let brandContent = document.createElement('p');
+    brandContent.textContent = brandname;
+    brand_Dashboard.appendChild(brandContent);
+})
--- opdracht4.js
+++ opdracht4.js
@@ -1,174 +1,27 @@
-// VOORRAAD ARRAY MET TV'S
-const inventory = [
-    {
-        type: '43PUS6504/12',
-        name: '4K TV',
-        brand: 'Philips',
-        price: 379,
-        availableSizes: [43, 50, 58, 65],
-        refreshRate: 50,
-        screenType: 'LED',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        options: {
-            wifi: true,
-            speech: false,
-            hdr: true,
-            bluetooth: false,
-            ambiLight: false,
-        },
-        originalStock: 23,
-        sold: 2,
-    },
-    {
-        type: 'NH3216SMART',
-        name: 'HD smart TV',
-        brand: 'Nikkei',
-        price: 159,
-        availableSizes: [32],
-        refreshRate: 100,
-        screenType: 'LED',
-        screenQuality: 'HD ready',
-        smartTv: true,
-        options: {
-            wifi: true,
-            speech: false,
-            hdr: false,
-            bluetooth: false,
-            ambiLight: false,
-        },
-        originalStock: 4,
-        sold: 4,
-    },
-    {
-        type: 'QE55Q60T',
-        name: '4K QLED TV',
-        brand: 'Samsung',
-        price: 709,
-        availableSizes: [43, 50, 55, 58, 65],
-        refreshRate: 60,
-        screenType: 'QLED',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        options: {
-            wifi: true,
-            speech: true,
-            hdr: true,
-            bluetooth: true,
-            ambiLight: false,
-        },
-        originalStock: 7,
-        sold: 0,
-    },
-    {
-        type: '43HAK6152',
-        name: 'Ultra HD SMART TV',
-        brand: 'Hitachi',
-        price: 349,
-        availableSizes: [43, 50, 55, 58],
-        refreshRate: 60,
-        screenType: 'LCD',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        options: {
-            wifi: true,
-            speech: true,
-            hdr: true,
-            bluetooth: true,
-            ambiLight: false,
-        },
-        originalStock: 5,
-        sold: 5,
-    },
-    {
-        type: '50PUS7304/12',
-        name: 'The One 4K TV',
-        brand: 'Philips',
-        price: 479,
-        availableSizes: [43, 50, 55, 58, 65, 70],
-        refreshRate: 50,
-        screenType: 'LED',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        options: {
-            wifi: true,
-            speech: true,
-            hdr: true,
-            bluetooth: true,
-            ambiLight: true,
-        },
-        originalStock: 8,
-        sold: 3,
-    },
-    {
-        type: '55PUS7805',
-        name: '4K LED TV',
-        brand: 'Philips',
-        price: 689,
-        availableSizes: [55],
-        refreshRate: 100,
-        screenType: 'LED',
-        screenQuality: 'Ultra HD/4K',
-        smartTv: true,
-        options: {
-            wifi: true,
-            speech: false,
-            hdr: true,
-            bluetooth: false,
-            ambiLight: true,
-        },
-        originalStock: 6,
-        sold: 3,
-    },
-    {
-        type: 'B2450HD',
-        name: 'LED TV',
-        brand: 'Brandt',
-        price: 109,
-        availableSizes: [24],
-        refreshRate: 60,
-        screenType: 'LED',
-        screenQuality: 'Full HD',
-        smartTv: false,
-        options: {
-            wifi: false,
-            speech: false,
-            hdr: false,
-            bluetooth: false,
-            ambiLight: false,
-        },
-        originalStock: 10,
-        sold: 8,
-    },
-    {
-        type: '32WL1A63DG',
-        name: 'HD TV',
-        brand: 'Toshiba',
-        price: 161,
-        availableSizes: [32],
-        refreshRate: 50,
-        screenType: 'LED',
-        screenQuality: 'Full HD',
-        smartTv: false,
-        options: {
-            wifi: false,
-            speech: false,
-            hdr: true,
-            bluetooth: false,
-            ambiLight: false,
-        },
-        originalStock: 10,
-        sold: 8,
-    },
-];
+// pseudocode: de voorraad staat in het Main JS bestand.
 
 // Opdracht 4: Schrijf de code uit 3 om naar een functie die een array met tv-objecten verwacht. Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken. Tip: vergeet deze functie -declaratie niet aan te roepen!
 
-const output_brand = inventory.map((tvtype,tvname,tvbrand,tvprice,tvavailableSizes, tvrefreshRate, tvscreenType, tvscreenQuality, tvsmartTv) => {
-    return brandname.brand;
-});
-console.log('* TV merken (8 resultaten met dubbele namen):',output_brand);
+//Maak/open een normale functie zonder parameter
+function brandList() {
+// Maak een array = hetzelfde als in opdracht 3:
+// het resultaat van de Map methode stop ik in een variabele 'output_brand_list'
 
-const tvList_Dashboard = document.getElementById("tv-list");
-console.log(tvList_Dashboard);
-tvList_Dashboard.textContent = output_brand;
+    const output_brand_list = inventory.map((brandname) => {
+        return brandname.brand;
+    });
+    console.log('* TV merken functie (alweer 8 resultaten met dubbele namen):',output_brand_list);
+
+// pseudocode: maak eerst tags in het HTML bestand, waarvan 1 een ID='brandlistOutput' heeft
+// dit wordt een 'node'
+    const brandlistOutputNode = document.getElementById("brandlistOutput");
+//zet elke merknaam in een <li> element. Zorg daarna dat deze in de juiste node komt met appendChild
+    output_brand_list.forEach(brandValue => {
+        let newBrandItem = document.createElement('li');
+        newBrandItem.textContent = brandValue;
+        brandlistOutputNode.appendChild(newBrandItem);
+    })
+}
+
+// roep de normale functie aan
+brandList();
